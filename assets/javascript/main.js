9390051f73b7a30b9db3c57530b14d8b5578a696
ndgithub <nickydesai.mail@gmail.com>
2019-02-23 21:47:09 -0600

    Added Portfolio section

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,31 +1,33 @@
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
-$(window).on('scroll', function () {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-});
+// $(window).on('scroll', function () {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// });
 
-var winWidth = $(window).width();
-console.log('winWidth', winWidth);
-function goldRatio(level) {
-  var dim = winWidth;
-  for (var i = 1; i < level; i++) {
-    dim *= 0.618;
-  }
-  return dim;
-}
+// var winWidth = $(window).width();
+// console.log('winWidth', winWidth);
+// function goldRatio(level) {
+//   var dim = winWidth;
+//   for (var i = 1; i < level; i++) {
+//     dim *= 0.618;
+//   }
+//   return dim;
+// }
 
-$('#navbar').width('600');
 
-$('#logo-container').width(goldRatio(3));
-$('#logo-container').css('margin-bottom', goldRatio(6));
-$('#logo-container').css('padding', goldRatio(9));
 
-$('#name').css('margin-bottom', goldRatio(100000));
+// $('#logo-container').width(goldRatio(3));
+// $('#logo-container').css('margin-bottom', goldRatio(6));
+// $('#logo-container').css('padding', goldRatio(9));
+
+// $(window).on('scroll', function () {
+//   console.log('hi');
+// })
 
 
 
